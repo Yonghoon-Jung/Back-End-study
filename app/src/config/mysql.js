bacd3a8e36ad257262a4e389a4896a9931f7f407
Yonghoon <yonghoonj98@gmail.com>
2022-02-17 22:59:32 +0900

    테스트

--- app/src/config/mysql.js
+++ app/src/config/mysql.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2/promise");
 
+const { DB_HOST, DB_DATABASE, DB_USER, DB_PSWORD } = process.env;
+
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PSWORD,
-  database: process.env.DB_DATABASE,
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PSWORD,
+  database: DB_DATABASE,
 });
 
 // db.connect();
