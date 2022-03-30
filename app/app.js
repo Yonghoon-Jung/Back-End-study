6cde6d198ab61ed3327d7276abc02245cec3e90e
Yonghoon <yonghoonj98@gmail.com>
2022-03-30 13:11:43 +0900

    WS 공부

--- app/app.js
+++ app/app.js
@@ -3,19 +3,36 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-
+const path = require("path");
+const http = require("http");
+const server = http.createServer(app);
+const socketIO = require("socket.io");
+const io = socketIO(server);
+const moment = require("moment");
 dotenv.config();
 
+app.use(express.static(path.join(__dirname, "src")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const server = require("./src/apis/server");
+io.on("connection", (socket) => {
+  socket.on("chatting", (data) => {
+    console.log(data);
+    io.emit("chatting", data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("서버온");
+});
+
+const ser = require("./src/apis/server");
 const auth = require("./src/apis/auth");
 
-app.use("/server", server);
+app.use("/server", ser);
 app.use("/auth", auth);
 app.use("/", (req, res) => {
   res.send("Hello World");
 });
 
-module.exports = app;
+// module.exports = app;
--- app/bin/www.js
+++ app/bin/www.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const app = require("../app");
-const PORT = process.env.PORT || 3000;
+// const app = require("../app");
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`${PORT} 포트에서 서버가 가동되었습니다.`);
-});
+// app.listen(PORT, () => {
+//   console.log(`${PORT} 포트에서 서버가 가동되었습니다.`);
+// });
